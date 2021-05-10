782f30ef1d56f40cc83bfa920f82f89a121f31c1
Marcos Lutero da Silva Rocha <lutero_rocha@outlook.com>
2021-05-10 08:19:45 -0300

    alterando a BASE_URL novamente

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://sds3-lutero.herokuapp.com/';
+export const BASE_URL = 'https://sds3-lutero.herokuapp.com';
 
 //não lista
 //export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080'
